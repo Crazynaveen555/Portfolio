9a0003a5e5a9048eb497c2f88e5308a703731a49
Naveenkumar.v <125992766+Crazynaveen555@users.noreply.github.com>
2025-03-10 15:34:06 +0530

    Update ContactSocial.jsx

--- src/components/contactMeSection/ContactSocial.jsx
+++ src/components/contactMeSection/ContactSocial.jsx
@@ -6,7 +6,7 @@ import { FaInstagram } from "react-icons/fa";
 const ContactSocial = () => {
   return (
     <div className="flex gap-4">
-      <SingleContactSocial link="www.linkedin.com/in/naveen-kumar-850520270" Icon={FaLinkedinIn} />
+      <SingleContactSocial link="https://www.linkedin.com/in/naveen-kumar-850520270?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app" Icon={FaLinkedinIn} />
       <SingleContactSocial link="https://github.com/Crazynaveen555" Icon={FiGithub} />
       <SingleContactSocial link="#" Icon={FaInstagram} />
     </div>
